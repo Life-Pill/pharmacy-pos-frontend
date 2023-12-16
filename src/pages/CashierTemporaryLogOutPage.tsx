6949963fb4307cfd22e46a501f06300ee65041b0
Tharindu Imalka <tharinduimalka915@gmail.com>
2023-12-16 19:17:37 +0530

    done:date and clock

---
+++ src/pages/CashierTemporaryLogOutPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import DateAndClock from '../components/clock/DateAndClock';
+
+type Props = {};
+
+const CashierTemporaryLogOutPage = (props: Props) => {
+  return (
+    <div>
+      {/* Clock */}
+      <DateAndClock />
+      {/* Date */}
+      {/* Recently logged cashier */}
+    </div>
+  );
+};
+
+export default CashierTemporaryLogOutPage;
