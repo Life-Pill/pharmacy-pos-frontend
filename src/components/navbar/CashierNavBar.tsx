24a32dcfe5e7d3261749e48af52a984e6b88452a
Tharindu Imalka <tharinduimalka915@gmail.com>
2023-12-17 13:21:10 +0530

    done: cashier navbar completed

--- src/components/navbar/CashierNavBar.tsx
+++ src/components/navbar/CashierNavBar.tsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import { IoArrowBackCircleOutline } from 'react-icons/io5';
+import DateAndTimeNavBar from '../clock/DateAndTimeNavBar';
+import MedicineSearchBar from '../searchbar/MedicineSearchBar';
+import { AiFillHome } from 'react-icons/ai';
+import ButtonWithIconAndText from '../buttons/ButtonWithIconAndText';
+import { CiViewList } from 'react-icons/ci';
+import { LiaHistorySolid } from 'react-icons/lia';
+import { PiDeviceMobileSpeakerThin } from 'react-icons/pi';
+import ProfileNameCard from '../cashierprofile/ProfileNameCard';
+import Divider from '../divider/Divider';
+const Logo = require('../../assets/logo/logo.png');
 
 type Props = {};
 
 const CashierNavBar = (props: Props) => {
-  return <div>CashierNavBar</div>;
+  const handleClick = () => {};
+  return (
+    <div className='flex items-center justify-between w-full p-2 mx-4 my-2 font-poppins shadow-md'>
+      {/* Back Button */}
+      <IoArrowBackCircleOutline size={40} style={{ color: 'gray' }} />
+
+      <Divider />
+
+      {/* Logo */}
+      <div>
+        <img src={Logo} alt='Logo' width={60} height={60} />
+      </div>
+
+      {/* Date and time */}
+      <DateAndTimeNavBar />
+
+      {/* Search */}
+      <MedicineSearchBar />
+
+      <Divider />
+
+      {/* Buttons for home,orders,history and online orders */}
+      <ButtonWithIconAndText
+        icon={<AiFillHome size={20} />}
+        text='Home'
+        onClick={handleClick}
+      />
+      <ButtonWithIconAndText
+        icon={<CiViewList size={20} />}
+        text='Orders'
+        onClick={handleClick}
+      />
+      <ButtonWithIconAndText
+        icon={<LiaHistorySolid size={20} />}
+        text='History'
+        onClick={handleClick}
+      />
+      <ButtonWithIconAndText
+        icon={<PiDeviceMobileSpeakerThin size={20} />}
+        text='Online Orders'
+        onClick={handleClick}
+      />
+
+      <Divider />
+
+      {/* Cashier name,number and profile picture */}
+      <ProfileNameCard />
+    </div>
+  );
 };
 
 export default CashierNavBar;
