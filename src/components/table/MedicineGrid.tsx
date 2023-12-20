40eaa8ae44d4aa5a8077b14524a0dfe37b5c6af8
Tharindu Imalka <tharinduimalka915@gmail.com>
2023-12-20 16:03:01 +0530

    fake database created for testing

--- src/components/table/MedicineGrid.tsx
+++ src/components/table/MedicineGrid.tsx
@@ -3,7 +3,20 @@ import React from 'react';
 type Props = {};
 
 const MedicineGrid = (props: Props) => {
-  return <div>MedicineGrid</div>;
+  return (
+    <div>
+      <table className='table-fixed'>
+        <thead>
+          <tr>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>{}</tbody>
+      </table>
+    </div>
+  );
 };
 
 export default MedicineGrid;
