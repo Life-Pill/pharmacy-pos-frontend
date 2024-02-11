188faea6cb71bae9d8e56243972575af10c764f7
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-02-11 09:09:32 +0530

    Add ManagerSidebar component to MainDashboard

--- src/features/manager-dashboard/layout/MainDashboard.tsx
+++ src/features/manager-dashboard/layout/MainDashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ManagerNavbar from '../components/navbar/ManagerNavbar';
+import ManagerSidebar from '../components/sidebar/ManagerSidebar';
 
 type Props = {};
 
@@ -7,6 +8,15 @@ const MainDashboard = (props: Props) => {
   return (
     <div>
       <ManagerNavbar />
+      <div className='flex flex-row'>
+        <ManagerSidebar />
+        <div className='flex flex-col w-full p-4 font-poppins'>
+          <h1 className='text-2xl font-semibold'>
+            Welcome to the Manager Dashboard
+          </h1>
+          <p className='text-lg'>This is the main dashboard for the manager</p>
+        </div>
+      </div>
     </div>
   );
 };
