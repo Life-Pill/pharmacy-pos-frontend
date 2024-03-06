4eabe3bf94304e1f1f711f0d0308e8e839d7fac2
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-03-07 02:29:54 +0530

    Add dependencies for react-chartjs-2 and chart.js and added bar chart and pie chart

---
+++ src/features/manager-dashboard/components/main/Dashboard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import BarGraph from '../../../../shared/charts/BarGraph';
+import branchSaleDetails from '../../../../assets/fakedata/branchsales';
+import DoughnutChart from '../../../../shared/charts/DoughnutChart';
+import branchOrderDetails from '../../../../assets/fakedata/orderdetails';
+type Props = {};
+
+const Dashboard = (props: Props) => {
+  return (
+    <div className=' w-full grid grid-cols-2 grid-rows-2 gap-8'>
+      <div className='p-4'>
+        <p className='text-md text-center font-bold'>Sales by Branch</p>
+        <BarGraph branchSaleDetails={branchSaleDetails} />
+      </div>
+      <div className='p-4'>
+        <p className='text-md text-center font-bold'>Order by Branch</p>
+        <DoughnutChart data={branchOrderDetails} />
+      </div>
+      <div className='p-4'>Item 3</div>
+      <div className='bg-gray-200 p-4'>Item 4</div>
+    </div>
+  );
+};
+
+export default Dashboard;
