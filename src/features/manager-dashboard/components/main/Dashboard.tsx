7a0e3f2cbe86c1a8df3d84c8f57040e0b357bd0e
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-03-10 06:50:42 +0530

    Implemented cashier report component

--- src/features/manager-dashboard/components/main/Dashboard.tsx
+++ src/features/manager-dashboard/components/main/Dashboard.tsx
@@ -20,9 +20,6 @@ const Dashboard = (props: Props) => {
       </div>
       <div className='p-4'>
         <CashierRecentTransactionCard />
-        <CashierRecentTransactionCard />
-        <CashierRecentTransactionCard />
-        <CashierRecentTransactionCard />
       </div>
       <div className='bg-gray-200 p-4'>
         <LatestTransactionDetails />
