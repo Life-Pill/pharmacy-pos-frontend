1e0f036c4215251e89aa37aa2d14b93a7b481f7b
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-03-24 22:46:06 +0530

    Added tables for personal care, medical device, first aid , sports

---
+++ src/features/cashier-dashboard/components/nutrition-table/NutritionTable.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import { IoIosAdd } from 'react-icons/io';
+import { IMedicine } from '../../../../interfaces/IMedicine';
+import CountRoundButton from '../../../../shared/buttons/CountRoundButton';
+import { usePaymentContext } from '../../layout/MainCashierDashboard';
+import getAllItems from '../../services/ItemService';
+import { MedicineType } from '../medicine-table/MedicineColumns';
+
+type Props = {};
+
+function NutritionTable({}: Props) {
+  const { orderedMedicine, setOrderedMedicine } = usePaymentContext();
+  const [medicine, setMedicine] = useState<IMedicine[]>([]);
+
+  //function to add medicine to ordered medicine
+  const handleAddClick = (medicine: MedicineType) => {
+    setOrderedMedicine([
+      ...orderedMedicine,
+      {
+        id: medicine.id,
+        name: medicine.name,
+        unitPrice: medicine.price,
+        amount: 0,
+        availableQuantity: medicine.quantity,
+      },
+    ]);
+  };
+
+  //
+  useEffect(() => {
+    //fetchMedicine from server
+    fetchMedicine();
+  }, []);
+  //
+
+  const fetchMedicine = async () => {
+    const allItems = await getAllItems();
+    //filter only personal care
+    const medicine = allItems.filter(
+      (item: IMedicine) => item.category === 'Nutrition'
+    );
+    setMedicine(medicine);
+  };
+
+  return (
+    <div className='max-h-[750px] overflow-y-scroll w-full'>
+      <table className='text-sm text-left text-gray-500 dark:text-gray-400 max-h-screen overflow-scroll w-full'>
+        <thead className='text-xs uppercase bg-slate-300 sticky top-0'>
+          <tr>
+            <th scope='col' className='px-6 py-3'>
+              Medicine ID
+            </th>
+            <th scope='col' className='px-6 py-3'>
+              Image
+            </th>
+            <th scope='col' className='px-6 py-3'>
+              Name
+            </th>
+            <th scope='col' className='px-6 py-3'>
+              Price
+            </th>
+            <th scope='col' className='px-6 py-3'>
+              Quantity
+            </th>
+            <th scope='col' className='px-6 py-3'>
+              Status
+            </th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {medicine.map((cashier) => (
+            <tr className='bg-slate-50 border-b'>
+              <td className='px-6 py-4'>{cashier.id}</td>
+              <td className='px-6 py-4 w-8 h-8'>
+                <img src={cashier.image} alt={cashier.name} />
+              </td>
+              <td className='px-6 py-4'>{cashier.name}</td>
+              <td className='px-6 py-4'>{cashier.price}</td>
+              <td className='px-6 py-4'>{cashier.quantity}</td>
+              <td className='px-6 py-4'>{cashier.status}</td>
+              <td className='px-6 py-4'>
+                <CountRoundButton
+                  onClick={() => handleAddClick(cashier)}
+                  icon={<IoIosAdd />}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default NutritionTable;
