be91d8fa125377de3bcab9b8ffde932cc782b414
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-06-02 15:05:47 +0530

    update port

--- src/features/login/services/useAxiosInstance.tsx
+++ src/features/login/services/useAxiosInstance.tsx
@@ -4,7 +4,7 @@ import { useUserContext } from '../../../context/UserContext';
 
 const useAxiosInstance = () => {
   const instance = axios.create({
-    baseURL: 'http://localhost:8081/lifepill/v1',
+    baseURL: 'http://localhost:8079/lifepill/v1',
     headers: {
       'Content-type': 'application/json',
     },
